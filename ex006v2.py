d00ee3919ef15b844c784a7798d95a7feb034bb6
Lucas Magalhães Rodrigues <127453899+iamlucasmagalhaes@users.noreply.github.com>
2024-12-03 22:48:42 -0300

    ex006v2

--- ex006v2.py
+++ ex006v2.py
@@ -6,10 +6,8 @@ def time_generator():
     return random.randint(3, 10)
 
 async def task(id):
-    print(f'Task iniciada {id}')
     time = time_generator()
     await asyncio.sleep(time)
-    
     print(f'Task finalizada: {id} em {time} segundos')
 
 async def main():
@@ -20,11 +18,11 @@ async def main():
     
     #cria 30 tarefas
     for i in range(1, 31):
-        task = asyncio.create_task(task(i))
-        tasks.append(task)
-    
-    #espera todas as tarefas terminarem
-    await asyncio.gather(*tasks)
+        task_obj = asyncio.create_task(task(i))
+        tasks.append(task_obj) 
+
+    #recebe a minha função tasks e retorna para o done as tarefas que foram completadas
+    done = await asyncio.wait(tasks, return_when=asyncio.FIRST_COMPLETED)
 
 #executa a função main
 asyncio.run(main())
