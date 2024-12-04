ed299d448256d0fe3023e6fbd42f500444606c5c
Lucas Magalhães Rodrigues <127453899+iamlucasmagalhaes@users.noreply.github.com>
2024-12-03 22:08:31 -0300

    ex006v1

--- ex006v1.py
+++ ex006v1.py
@@ -1,15 +1,16 @@
 import asyncio
+import random
 
+#retorna um número aleatório entre 3 e 10
 def time_generator():
-    
+    return random.randint(3, 10)
 
-async def task1(id):
+async def task(id):
     print(f'Task iniciada {id}')
-    if id % 2 == 0:
-        await asyncio.sleep(4)
-    await asyncio.sleep(6)
+    time = time_generator()
+    await asyncio.sleep(time)
     
-    print(f'Task finalizada: {id}')
+    print(f'Task finalizada: {id} em {time} segundos')
 
 async def main():
     print('Aplicação iniciada')
@@ -19,7 +20,7 @@ async def main():
     
     # Cria 30 tarefas
     for i in range(1, 31):
-        task = asyncio.create_task(task1(i))
+        task = asyncio.create_task(task(i))
         tasks.append(task)
     
     # Espera todas as tarefas terminarem
